a5c1a6e586520ed692ab75b9168e18f94d875927
GITC <gitcomputerss@gmail.com>
2024-05-31 15:35:20 +0100

    Initial commit

---
+++ ap.py
@@ -0,0 +1,14 @@
+import streamlit as st
+import pandas as pd
+import  numpy as np
+
+#configuring the page along with the tab icon
+st.set_page_config(page_title="Superstore Analysis", page_icon=":chart_with_upwards_trend:", layout="wide")
+
+#display text on the page
+st.write("Hello world")
+
+st.text('Fixed width text')
+st.title('My title')
+st.header('My header')
+st.subheader('My sub')
